893b3c3edb71482979a13e0146968d0a78d8ceb1
franagan <118491675+franagan@users.noreply.github.com>
2023-03-14 01:07:53 +0100

    all

---
+++ Iteración #3 Operadores/main.js
@@ -0,0 +1,24 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+// let a = 10;
+// let b = 5;
+// alert(a*b);
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+// let a = 10;
+// let b = 2;
+// alert(a/b);
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+// let a = 15;
+// let b = 9;
+// alert(a%b);
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+// let x = 15;
+// let y = 10;
+// let z = 5;
+// x = y + z;
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+// let x = 50;
+// let y = 10;
+// let z = 5;
+// x = y * z;
---
+++ Iteración #6 Bucles/exercise-3.js
@@ -0,0 +1,5 @@
+
+for(let contandoOvejas=0;contandoOvejas <10; contandoOvejas++){
+    if(contandoOvejas < 9){ console.log('Intentando dormir') }
+    else{ console.log('Dormido!')}
+ }
